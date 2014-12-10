b7743540b9a670870281ae5cc6cabaaf5c822b31
Josh Braegger <rckclmbr@gmail.com>
2014-12-09 23:47:55 -0700

    spec: default digest format is sha512
    Currently the ACI spec leverages sha256 as the default digest format, which
    is not as performant on 64-bit systems. A modern 64-bit processor can compute
    a SHA-512 hash in about 50-75% of the time it takes to create a SHA-256 hash.
    (This is because SHA-512 primarily uses 64-bit data types.) Considering that
    container images can become rather large, this is substantial.
    
    Fixes #8

--- schema/types/hash.go
+++ schema/types/hash.go
@@ -1,7 +1,7 @@
 package types
 
 import (
-	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -9,11 +9,16 @@ import (
 	"strings"
 )
 
+const (
+	maxHashSize = (sha512.Size / 2) + len("sha512-")
+)
+
 // Hash encodes a hash specified in a string of the form:
 //    "<type>-<value>"
 // for example
-//    "sha256-06c733b1838136838e6d2d3e8fa5aea4c7905e92"
-// Valid types are currently "sha256"
+//    "sha512-06c733b1838136838e6d2d3e8fa5aea4c7905e92[...]"
+// Valid types are currently:
+//  * sha512
 type Hash struct {
 	typ string
 	Val string
@@ -52,7 +57,7 @@ func (h Hash) Empty() bool {
 
 func (h Hash) assertValid() error {
 	switch h.typ {
-	case "sha256":
+	case "sha512":
 	case "":
 		return fmt.Errorf("unexpected empty hash type")
 	default:
@@ -84,9 +89,16 @@ func (h Hash) MarshalJSON() ([]byte, error) {
 	return json.Marshal(h.String())
 }
 
-func NewHashSHA256(b []byte) *Hash {
-	h := sha256.New()
+func NewHashSHA512(b []byte) *Hash {
+	h := sha512.New()
 	h.Write(b)
-	nh, _ := NewHash(fmt.Sprintf("sha256-%x", h.Sum(nil)))
+	nh, _ := NewHash(fmt.Sprintf("sha512-%x", h.Sum(nil)))
 	return nh
+}
+
+func ShortHash(hash string) string {
+	if len(hash) > maxHashSize {
+		return hash[:maxHashSize]
+	}
+	return hash
 }
