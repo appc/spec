329113447f27b57e02e98a3fc4b943f0c9de5a83
Maciej Pasternacki <maciej@3ofcoins.net>
2014-12-11 12:37:02 +0000

    schema: expand OS/arch whitelist to allow other platforms
    Introduced a map of valid OS/architecture combinations to allow
    implementations supporting platforms other than linux/amd64. I can't
    figure out at the moment how to warn rather than fail during JSON
    validation without resorting to "just print to stderr" approach. A
    workaround for an implementation would be to add own values to the
    `ValidOSArch` map, but this would still not allow full flexibility.
    
    Additionally fixed a typo in Labels.UnmarshalJSON that prevented the
    validation from actually being executed (an empty `l` Labels were
    validated, rather than deserialized `nl`).
    
    Improves #6 (not a full fix, as unlisted combinations still fail)

---
+++ schema/types/labels_test.go
@@ -0,0 +1,50 @@
+package types
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+)
+
+func TestLabels(t *testing.T) {
+	tests := []struct {
+		in        string
+		errPrefix string
+	}{
+		{
+			`[{"name": "os", "val": "linux"}, {"name": "arch", "val": "amd64"}]`,
+			"",
+		},
+		{
+			`[{"name": "os", "val": "freebsd"}, {"name": "arch", "val": "amd64"}]`,
+			"",
+		},
+		{
+			`[{"name": "os", "val": "OS/360"}, {"name": "arch", "val": "S/360"}]`,
+			`bad os "OS/360"`,
+		},
+		{
+			`[{"name": "os", "val": "linux"}, {"name": "arch", "val": "arm"}]`,
+			`bad arch "arm" for linux`,
+		},
+		{
+			`[]`,
+			"",
+		},
+	}
+	for i, tt := range tests {
+		var l Labels
+		if err := json.Unmarshal([]byte(tt.in), &l); err != nil {
+			if tt.errPrefix == "" {
+				t.Errorf("#%d: got err=%v, expected no error", i, err)
+			} else if !strings.HasPrefix(err.Error(), tt.errPrefix) {
+				t.Errorf("%#d: got err=%v, expected prefix %#v", i, err, tt.errPrefix)
+			}
+		} else {
+			t.Log(l)
+			if tt.errPrefix != "" {
+				t.Errorf("%#d: got no err, expected prefix %#v", i, tt.errPrefix)
+			}
+		}
+	}
+}
