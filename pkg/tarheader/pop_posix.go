85dac8ebb4069373c206e39ca0c0feb6dd5cdebe
Alban Crequy <alban@endocode.com>
2015-01-16 16:50:07 +0100

    tarheader: correctly set major/minor on /dev files

--- pkg/tarheader/pop_posix.go
+++ pkg/tarheader/pop_posix.go
@@ -17,6 +17,11 @@ func populateHeaderUnix(h *tar.Header, fi os.FileInfo, seen map[uint64]string) {
 	}
 	h.Uid = int(st.Uid)
 	h.Gid = int(st.Gid)
+	if st.Mode&syscall.S_IFMT == syscall.S_IFBLK || st.Mode&syscall.S_IFMT == syscall.S_IFCHR {
+		// FIXME: it would be better to use the C macros: major(3), minor(3)
+		h.Devminor = int64(st.Rdev & 0xff)
+		h.Devmajor = int64((st.Rdev >> 8) & 0xfff)
+	}
 	// If we have already seen this inode, generate a hardlink
 	p, ok := seen[uint64(st.Ino)]
 	if ok {
