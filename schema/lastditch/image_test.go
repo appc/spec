68998c1858b89aa105ae5fe838f4443d9dc7e609
Krzesimir Nowak <qdlacz@gmail.com>
2015-09-30 10:21:43 +0200

    last-ditch: Add labels to image manifest
    May be useful for extracting image version.

--- schema/lastditch/image_test.go
+++ schema/lastditch/image_test.go
@@ -15,35 +15,61 @@
 package lastditch
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
-
-	"github.com/appc/spec/schema/types"
 )
 
-func TestImageManifestWithInvalidName(t *testing.T) {
-	invalidName := "example.com/test!"
-	imj := `
+func TestInvalidImageManifest(t *testing.T) {
+	tests := []struct {
+		desc     string
+		json     string
+		expected ImageManifest
+	}{
 		{
-		    "acKind": "ImageManifest",
-		    "acVersion": "0.7.0",
-		    "name": "` + invalidName + `"
+			desc:     "Check an empty image manifest",
+			json:     imgJ("", labsJ(), ""),
+			expected: imgI("", labsI()),
+		},
+		{
+			desc:     "Check an image manifest with an empty label",
+			json:     imgJ("example.com/test!", labsJ(labJ("", "", "")), ""),
+			expected: imgI("example.com/test!", labsI(labI("", ""))),
+		},
+		{
+			desc:     "Check an image manifest with an invalid name",
+			json:     imgJ("example.com/test!", labsJ(), ""),
+			expected: imgI("example.com/test!", labsI()),
+		},
+		{
+			desc:     "Check an image manifest with labels with invalid names",
+			json:     imgJ("im", labsJ(labJ("!n1", "v1", ""), labJ("N2~", "v2", "")), ""),
+			expected: imgI("im", labsI(labI("!n1", "v1"), labI("N2~", "v2"))),
+		},
+		{
+			desc:     "Check an image manifest with duplicated labels",
+			json:     imgJ("im", labsJ(labJ("n1", "v1", ""), labJ("n1", "v2", "")), ""),
+			expected: imgI("im", labsI(labI("n1", "v1"), labI("n1", "v2"))),
+		},
+		{
+			desc:     "Check an image manifest with some extra fields",
+			json:     imgJ("im", labsJ(), extJ("stuff")),
+			expected: imgI("im", labsI()),
+		},
+		{
+			desc:     "Check an image manifest with a label containing some extra fields",
+			json:     imgJ("im", labsJ(labJ("n1", "v1", extJ("clutter"))), extJ("stuff")),
+			expected: imgI("im", labsI(labI("n1", "v1"))),
+		},
+	}
+	for _, tt := range tests {
+		got := ImageManifest{}
+		if err := got.UnmarshalJSON([]byte(tt.json)); err != nil {
+			t.Errorf("%s: unexpected error during unmarshalling image manifest: %v", tt.desc, err)
 		}
-		`
-	if types.ValidACIdentifier.MatchString(invalidName) {
-		t.Fatalf("%q is an unexpectedly valid name", invalidName)
-	}
-	expected := ImageManifest{
-		ACKind:    "ImageManifest",
-		ACVersion: "0.7.0",
-		Name:      invalidName,
-	}
-	im := ImageManifest{}
-	if err := im.UnmarshalJSON([]byte(imj)); err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-	if !reflect.DeepEqual(im, expected) {
-		t.Errorf("did not get expected image manifest, got: %+v, expected: %+v", im, expected)
+		if !reflect.DeepEqual(tt.expected, got) {
+			t.Errorf("%s: did not get expected image manifest, got:\n  %#v\nexpected:\n  %#v", tt.desc, got, tt.expected)
+		}
 	}
 }
 
@@ -66,5 +92,27 @@ func TestBogusImageManifest(t *testing.T) {
 		if im.UnmarshalJSON([]byte(str)) == nil {
 			t.Errorf("bogus image manifest unmarshalled successfully: %s", str)
 		}
+	}
+}
+
+// imgJ returns an image manifest JSON with given name and labels
+func imgJ(name, labels, extra string) string {
+	return fmt.Sprintf(`
+		{
+		    %s
+		    "acKind": "ImageManifest",
+		    "acVersion": "0.7.0",
+		    "name": "%s",
+		    "labels": %s
+		}`, extra, name, labels)
+}
+
+// imgI returns an image manifest instance with given name and labels
+func imgI(name string, labels Labels) ImageManifest {
+	return ImageManifest{
+		ACVersion: "0.7.0",
+		ACKind:    "ImageManifest",
+		Name:      name,
+		Labels:    labels,
 	}
 }
