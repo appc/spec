36151571badda04ec697ba72708483ddaca02344
Iago López Galeiras <iaguis@gmail.com>
2015-10-01 15:51:55 +0200

    actool: add supplementary-groups to help

--- actool/manifest.go
+++ actool/manifest.go
@@ -65,6 +65,7 @@ var (
 		  [--capability=CAP_SYS_ADMIN,CAP_NET_ADMIN]
 		  [--mounts=work,path=/opt,readOnly=true[:work2,...]]
 		  [--ports=query,protocol=tcp,port=8080[:query2,...]]
+		  [--supplementary-groups=gid1,gid2,...]
 		  [--isolators=resource/cpu,request=50m,limit=100m[:resource/memory,...]]
 		  [--replace]
 		  INPUT_ACI_FILE
