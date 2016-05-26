78a9e4389a36cc28d382891e32ee0148d81a18a4
Stefan Junker <mail@stefanjunker.de>
2016-05-26 11:41:18 +0200

    deps: k8s.io/kubernetes: use v1.3.0-alpha.4

--- Godeps/_workspace/src/k8s.io/kubernetes/pkg/api/resource/deep_copy_generated.go
+++ Godeps/_workspace/src/k8s.io/kubernetes/pkg/api/resource/deep_copy_generated.go
@@ -21,27 +21,35 @@ limitations under the License.
 package resource
 
 import (
+	inf_v0 "gopkg.in/inf.v0"
 	conversion "k8s.io/kubernetes/pkg/conversion"
 )
 
 func DeepCopy_resource_Quantity(in Quantity, out *Quantity, c *conversion.Cloner) error {
-	if newVal, err := c.DeepCopy(in.i); err != nil {
-		return err
+	if in.Amount != nil {
+		in, out := in.Amount, &out.Amount
+		*out = new(inf_v0.Dec)
+		if newVal, err := c.DeepCopy(*in); err != nil {
+			return err
+		} else {
+			**out = newVal.(inf_v0.Dec)
+		}
 	} else {
-		out.i = newVal.(int64Amount)
+		out.Amount = nil
 	}
-	if newVal, err := c.DeepCopy(in.d); err != nil {
-		return err
-	} else {
-		out.d = newVal.(infDecAmount)
-	}
-	if in.s != nil {
-		in, out := in.s, &out.s
+	out.Format = in.Format
+	return nil
+}
+
+func DeepCopy_resource_QuantityProto(in QuantityProto, out *QuantityProto, c *conversion.Cloner) error {
+	out.Format = in.Format
+	out.Scale = in.Scale
+	if in.Bigint != nil {
+		in, out := in.Bigint, &out.Bigint
 		*out = make([]byte, len(in))
 		copy(*out, in)
 	} else {
-		out.s = nil
+		out.Bigint = nil
 	}
-	out.Format = in.Format
 	return nil
 }
